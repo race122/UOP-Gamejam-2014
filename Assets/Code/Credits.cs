02bb430d72c3a019795f252f011a76aebde3ac58
floorman <daniel.guy.moody@gmail.com>
2014-06-13 00:33:11 +0100

    Re-doing credits menu and other updates to main scene

---
+++ Assets/Code/Credits.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+	private float cameraSpeed = -1.0f;
+
+	void Start() {
+	}
+	
+	void Update() {
+		if ( Input.GetKeyDown( KeyCode.Escape ) ) {
+			Application.LoadLevel( "MainMenu" );
+		}
+
+		camera.transform.translate( 0f, cameraSpeed, 0f );
+	}
+}
