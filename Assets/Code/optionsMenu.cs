fb3e100c37895dce059c290ac2cc01a5a368dd27
vulpenie1 <vulpenie1@hotmail.com>
2014-06-12 09:55:52 +0100

    commit

---
+++ Assets/Code/optionsMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class optionsMenu : MonoBehaviour {
+
+	public float newVolume;
+
+	// Use this for initialization
+	void Start () {
+		newVolume = GameManager.getVolume() * 100;
+		Debug.Log (newVolume);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI() {
+		newVolume = GUI.HorizontalSlider(new Rect( ( Screen.width / 2 ) - 150, ( Screen.height / 2 ) , 300, 150), newVolume, 0.0F, 100.0F);
+
+		GameManager.setVolume( ( newVolume ) );
+
+		GUI.Label( new Rect( ( Screen.width / 2 ) - 50, ( Screen.height / 2 ) - 25 , 300, 25 ), "Volume: " + newVolume);
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+UOP-Gamejam-2014
+================
+
+Geri-Lynn Ramsay's Xtreme Curling 2014
+
+To Do List:
+
+* Sound
+ * Implement placeholder sounds ( mostly done )
+* UI
+ * Score (each player)
+ * Turn
+ * Current team?
+* Gameplay
+ * Win condition
+ * 2nd player
+ * Brushing
+ * Power of rock throw
